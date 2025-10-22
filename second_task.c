b6b4ec4e7918e80e85a9d900e9e8e8b736b48eaf
Feri0726 <feherferi2007@gmail.com>
2025-10-22 15:17:39 +0000

    2 and 3 sznc

--- second_task.c
+++ second_task.c
@@ -3,11 +3,23 @@ Write a function that receives the length of an edge of a cube as a parameter. T
 (The return type of the function is void.) Call this function from the main to calculate the volume and the surface area of a cube of 2.7 edge length, then print the results in main!
 
 */
-void cube(double edge, double *parea, double *pvolume);
-
 #include <stdio.h>
+void cube(double edge, double *parea, double *pvolume)
+{
+    *parea = (edge*edge*6);
+    *pvolume = (edge*edge*edge);
+}
+
 
 int main(){
+    double edge;
+    double par;
+    double pvo;
+    printf("Edge?");
+    scanf("%lf", &edge);
+    cube(edge, &par, &pvo);
+    printf("A %f long edge cubes parea is %f, volume is %f", edge, par, pvo);
+
 
 return 0;
 }
--- third_task.c
+++ third_task.c
@@ -2,10 +2,21 @@
 Write a function that receives two numbers, and return !!both!! their sum and their product!
 one function has to do that!
 */
-
+#include <stdio.h>
+double sumerator(double a, double b, double *sum, double *prod){
+    *sum = a + b;
+    *prod = a * b;
+}
 
 
 int main(void) {
+    double a, b, sum, prod;
+    printf("A and B?");
+    scanf("%lf", &a );
+    scanf("%lf", &b );
+    sumerator(a, b, &sum, &prod);
+    printf("Values: %f, %f; Their sum and prod: %f, %f", a, b, sum, prod);
+
    
 
     return 0;
